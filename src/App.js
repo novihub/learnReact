1057916db22cac59a74bd2ee9faabc472fe2c5f3
Novikoff <novikoff04@gmail.com>
2024-07-04 17:47:07 +0500

    route exact

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react'
-import './App.css'
-import Header from './components/Header/Header'
-import Nav from './components/Nav/Nav'
-import Profile from './components/Profile/Profile'
-import Dialogs from './components/Dialogs/Dialogs'
+// App.js
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import Nav from './components/Nav/Nav';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
-	return (
-		<BrowserRouter>
-			<div className='app-wrapper'>
-				<Header />
-				<Nav />
-				<div className='app-wrapper-content'>
-					<Routes>
-						<Route path='/profile' Component={Profile} />
-						<Route path='/messages' Component={Dialogs} />
-					</Routes>
-				</div>
-			</div>
-		</BrowserRouter>
-	)
+  return (
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Nav />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/messages/*' element={<Dialogs />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,10 +2,33 @@ import React from 'react'
 
 import classes from './Dialogs.module.css'
 
-const Dialogs = (props) => {
+import { NavLink } from 'react-router-dom'
+
+const Dialogs = props => {
 	return (
-		<div className={classes.content}>
-			Dialogs12312312
+		<div className={classes.dialogs}>
+			<div className={classes.dialogItems}>
+				<div className={classes.dialog}>
+					<NavLink to='/messages/1'>First</NavLink>
+				</div>
+				<div className={classes.dialog}>
+					<NavLink to='/messages/2'>Second</NavLink>
+				</div>
+				<div className={classes.dialog}>
+					<NavLink to='/messages/3'>Third</NavLink>
+				</div>
+				<div className={classes.dialog}>
+					<NavLink to='/messages/4'>Fourth</NavLink>
+				</div>
+				<div className={classes.dialog}>
+					<NavLink to='/messages/5'>Fifth</NavLink>
+				</div>
+			</div>
+			<div className={classes.messages}>
+				<div className={classes.message}>Hi</div>
+				<div className={classes.message}>Hello</div>
+				<div className={classes.message}>How are you</div>
+			</div>
 		</div>
 	)
 }
