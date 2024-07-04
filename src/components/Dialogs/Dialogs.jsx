7633743b1fe6a7decbc21b4c66ec509da2bc7812
Novikoff <novikoff04@gmail.com>
2024-07-04 22:42:47 +0500

    added method map, refactoring

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -19,7 +19,7 @@ const Message = props => {
 }
 
 const Dialogs = () => {
-	let dialogsData = [
+	let dialogs = [
 		{ id: 1, name: 'Dimych' },
 		{ id: 2, name: 'Sasha' },
 		{ id: 3, name: 'Max' },
@@ -28,26 +28,25 @@ const Dialogs = () => {
 		{ id: 5, name: 'Alina' }
 	]
 
-	let messagesData = [
+	let messages = [
 		{ id: 1, message: 'Hi' },
 		{ id: 2, message: 'Hello' },
 		{ id: 3, message: 'How are you' },
 		{ id: 4, message: 'Where are you from' },
 		{ id: 5, message: 'Whats up' },
-		{ id: 6, message: 'Bye' },
+		{ id: 6, message: 'Bye' }
 	]
+
+	let dialogsElements = dialogs.map(dialog => (
+		<DialogItem name={dialog.name} id={dialog.id} />
+	))
+
+	let messagesElements = messages.map(m => <Message message={m.message} />)
 
 	return (
 		<div className={classes.dialogs}>
-			<div className={classes.dialogItems}>
-				<DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-				<DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-
-			</div>
-			<div className={classes.messages}>
-				<Message message={messagesData[0].message} />
-				<Message message={messagesData[1].message} />
-			</div>
+			<div className={classes.dialogItems}>{dialogsElements}</div>
+			<div className={classes.messages}>{messagesElements}</div>
 		</div>
 	)
 }
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,12 +4,16 @@ import classes from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = () => {
-	let postsData = [
+	let posts = [
 		{ id: 0, message: 'First', likesCount: 1 },
 		{ id: 1, message: 'Second', likesCount: 12 },
 		{ id: 2, message: 'Third', likesCount: 123 },
 		{ id: 3, message: 'Fourth', likesCount: 1234 }
 	]
+
+	let postsElements = posts.map(post => (
+		<Post message={post.message} likesCount={post.likesCount} />
+	))
 
 	return (
 		<div className={classes.postsBlock}>
@@ -17,11 +21,7 @@ const MyPosts = () => {
 				<textarea name='' id=''></textarea>
 				<button>Add post</button>
 			</div>
-			<div className={classes.posts}>
-				<Post message={postsData[0].message} likesCount={postsData[0].likesCount} />
-				<Post message={postsData[1].message} likesCount={postsData[1].likesCount} />
-				<Post message={postsData[2].message} likesCount={postsData[2].likesCount} />
-			</div>
+			<div className={classes.posts}>{postsElements}</div>
 		</div>
 	)
 }
