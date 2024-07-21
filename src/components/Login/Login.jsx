ec957530d3b99833ea8e0f99ae64a6e4d1aaaa1a
novikoff <novikoff04@gmail.com>
2024-07-21 21:05:04 +0500

    implemented legacy redux form (login)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,41 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 import classes from './Login.module.css'
 
+const LoginForm = props => {
+	console.log(props)
+	return (
+		<form onSubmit={props.handleSubmit}>
+			<div>
+				<Field placeholder='Login' name='login' component='input' />
+			</div>
+			<div>
+				<Field placeholder='Password' name='password' component='input' />
+			</div>
+			<div>
+				<Field type='checkbox' name='rememberMe' component='input' />
+			</div>
+			<div>
+				<button>Login</button>
+			</div>
+		</form>
+	)
+}
+
+const LoginReduxForm = reduxForm({
+	form: 'login'
+})(LoginForm)
+
 const Login = props => {
+	const onSubmit = (formData) => {
+		console.log(formData)
+	}
+
 	return (
 		<div className={classes.Login}>
 			<h1>Access denied! You need to login :(</h1>
+			<LoginReduxForm onSubmit={onSubmit}/>
 		</div>
 	)
-}	
-
+}
 export default Login
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,15 +1,21 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
+import {
+	applyMiddleware,
+	combineReducers,
+	legacy_createStore as createStore
+} from 'redux'
+import { reducer as formReducer } from 'redux-form'
+import thunk from 'redux-thunk'
+import authReducer from './auth-reducer'
 import dialogsReducer from './dialogs-reducer'
 import profileReducer from './profile-reducer'
 import usersReducer from './users-reducer'
-import authReducer from './auth-reducer'
-import { thunk } from 'redux-thunk'
 
 let reducers = combineReducers({
 	profilePage: profileReducer,
 	dialogsPage: dialogsReducer,
 	usersPage: usersReducer,
-	auth: authReducer
+	auth: authReducer,
+	form: formReducer
 })
 
 let store = createStore(reducers, applyMiddleware(thunk))
